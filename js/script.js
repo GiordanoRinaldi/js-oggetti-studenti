b432256a5ff8cb45b6fea1e3608f7e80e1ff5b92
GiordanoRinaldi <giordanorinaldi0@gmail.com>
2021-09-07 16:01:33 +0200

    ciclato su gli studenti

--- js/script.js
+++ js/script.js
@@ -4,7 +4,7 @@ var studente = {
     nome: "Matteo",
     cognome: "Sormas",
     eta: 22
-}
+};
 
 console.log(studente);
 
@@ -12,9 +12,46 @@ console.log(studente);
 
 for (var key in studente){
     console.log(`${key} => ${studente[key]}`);
-}
+};
 
 //Creare un array di oggetti di studenti.
+
+var studenti = [
+    {
+        nome: "Matteo",
+        cognome: "Sormas",
+        eta: 22
+    },
+
+    {
+        nome: "Giordano",
+        cognome: "Rinaldi",
+        eta: 23
+    },
+
+    {
+        nome: "Alessandro",
+        cognome: "Firmani",
+        eta: 23
+    },
+
+    {
+        nome: "Francesco",
+        cognome: "Vittorio",
+        eta: 25
+    }
+];
+
+
+
 //Ciclare su tutti gli studenti e stampare per ognuno di essi, nome e cognome.
+
+for (var i = 0; i < studenti.length; i++) {
+    
+    console.log(studenti[i]["nome"]);
+    console.log(studenti[i]["cognome"]);
+    
+};
+
 //Dare la possibilità all’utente, attraverso 3 prompt(), di aggiungere un nuovo oggetto studente inserendo nell’ordine: nome, cognome e età.
 
